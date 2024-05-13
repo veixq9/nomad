be35eae676e3374176709828e1d89748b4aae9a5
veixq9 <134114824+veixq9@users.noreply.github.com>
2024-05-13 14:05:57 +0200

    Update nomad.clj

--- nomad.clj
+++ nomad.clj
@@ -1,19 +1,21 @@
-(def data
-  {
-   :links ["https://kaeltehilfe-berlin.de/"
-           "https://www.youtube.com/@SoftWhiteUnderbelly"
-           "https://www.amazon.de/-/en/Giorgio-Agamben/dp/0804732183"
-           "https://www.amazon.de/Ausnahmezustand-Homo-sacer-II-1-suhrkamp/dp/3518123661/ref=sr_1_1?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2G0HUZ0CFUPI2&dib=eyJ2IjoiMSJ9.TEiiI8ND6-q1DtuaZEMtuKVrDuBSHM_wOLHjbA7ylmd8vW2nRTIq0MFnJvrPuliVdhLBTjDmv69jc7GNE2vqwwDg5y6aYZwpZtkGYFLQVUi884nzxEdS5HdcG8N2oMOd65VzF7RcaQO5Z1seBdfzFAi6PhE1bx5DUlGpvFcIHHg2_oew2UCvfLwWl6oubCioH8-9npuvDFhRjTnbtsedI1GQLJBCi8xesJsLmLJ8_uk.GIoUHDCL8vF_deIDSkB4DhYeEHOn2nDIpZ_VyMr_1Ks&dib_tag=se&keywords=ausnahmezustand+agamben&qid=1712314012&sprefix=ausnahmezustand+agambe%2Caps%2C165&sr=8-1"]
-   :pre-processed "** link to discussion of charity-control-systems of biopolitical trickle-piss-crucifix-economy of control-state-machines goes here together with pictures of rabies infected EZB---crime--laundering-machine and færændly pisstitmother carrot re-warded ordnungs-next Queued-bots, not to mention the toorchish actress gartenzwerg \"oh i can't see\" konzentrations-lager-base-ball lawyer having the suppressed end up at respielraum with control in their hands **"
+{:id "c40159ea-7f12-421c-a497-9e074098130f",
+ :timestamp 1715601518419,
+ :body
+ (let [title "nomad"
+       title-note "when politics of geo-territorial states ..."
+       data {
+             :links {"https://kaeltehilfe-berlin.de/" #{:berlin}
+                     "https://unter-druck.de" #{:berlin}
+                     "https://www.youtube.com/@SoftWhiteUnderbelly" nil
+                     "https://www.amazon.de/-/en/Giorgio-Agamben/dp/0804732183" nil
+                     "https://www.amazon.de/Ausnahmezustand-Homo-sacer-II-1-suhrkamp/dp/3518123661/ref=sr_1_1?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2G0HUZ0CFUPI2&dib=eyJ2IjoiMSJ9.TEiiI8ND6-q1DtuaZEMtuKVrDuBSHM_wOLHjbA7ylmd8vW2nRTIq0MFnJvrPuliVdhLBTjDmv69jc7GNE2vqwwDg5y6aYZwpZtkGYFLQVUi884nzxEdS5HdcG8N2oMOd65VzF7RcaQO5Z1seBdfzFAi6PhE1bx5DUlGpvFcIHHg2_oew2UCvfLwWl6oubCioH8-9npuvDFhRjTnbtsedI1GQLJBCi8xesJsLmLJ8_uk.GIoUHDCL8vF_deIDSkB4DhYeEHOn2nDIpZ_VyMr_1Ks&dib_tag=se&keywords=ausnahmezustand+agamben&qid=1712314012&sprefix=ausnahmezustand+agambe%2Caps%2C165&sr=8-1" nil}
+             :pre-processed "** link to discussion of charity-control-systems of biopolitical trickle-piss-crucifix-economy of control-state-machines goes here together with pictures of rabies infected EZB---crime--laundering-machine and færændly pisstitmother carrot re-warded ordnungs-next Queued-bots, not to mention the toorchish actress gartenzwerg \"oh i can't see\" konzentrations-lager-base-ball lawyer having the suppressed end up at respielraum with control in their hands **"
 
-   :necessities '("Sleeping bag" "Hammock" "tent" "Boots" "Underwear" "Socks" "Handschuhe" "Zahnseide" "Toothbrush" "Mundwasch" "" "Lighter" "Gas kocher capsule" "Alu folie" "Swiss messer" "Flashlight" "Pen" "Paper" "Cash" "First aid kit" "Aspirin" "Taschentücher" "Sim card" "Phone and extra battery" "Biscuits" "subversion of control" "batbase")
-   
-   })
-
-(let [title "homeless/necessities"
-      title-note "when politics of slave labour and real-estate price-manipulation drives people into trashcans"]
-  {:html [:div
-          [:h1 title]
-          [:p.w3-bold-xxlarge title-note]
-          [:p 
-           (-> data :necessities)]]})
+             :necessities '("Sleeping bag" "Hammock" "tent" "Boots" "Underwear" "Socks" "Handschuhe" "Zahnseide" "Toothbrush" "Mundwasch" "" "Lighter" "Gas kocher capsule" "Alu folie" "Swiss messer" "Flashlight" "Pen" "Paper" "Cash" "First aid kit" "Aspirin" "Taschentücher" "Sim card" "Phone and extra battery" "Biscuits" "subversion of control" "batbase")}]
+   [:div
+    [:h1 title]
+    [:p.w3-bold-xxlarge title-note]
+    [:p 
+     (-> data :necessities)]])
+ :actor/public-key-ed25519
+ "0xec077faa8a230c86aa0b958c137a25a1dadae77c69edca95d2a1d5045ba79ca8"}
